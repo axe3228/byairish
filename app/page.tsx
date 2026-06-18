5215cd7ee655fea6c88c05fe29fd1545199d28b4
Archie Habaradas <archiehabaradas3228@gmail.com>
2026-06-19 04:55:41 +0800

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-full flex-col items-center justify-center p-8">
+      <h1 className="text-3xl font-semibold">byairish</h1>
+    </main>
+  );
+}
